f73185dfc411339e6708e2aa3b52d0015f5ea78d
Matt Jennings <matt@mattjennings.net>
2023-09-16 15:17:02 -0700

    added react-router-dom

--- react/src-react/components/App/App.tsx
+++ react/src-react/components/App/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import lowerCase from '../../utils/lowercase/lowercase';
-import ClickCounter from '../ClickCounter/ClickCounter';
-import image from '../../images/react-logo.png';
-import cover from '../../images/cover.svg';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../Home/Home';
 
 export default function App() {
   return (
-    <div>
-      <h1>{lowerCase('My React + Webpack Template')}</h1>
-      <h2>PNG Image</h2>
-      <p>
-        <img src={image} alt="React Logo" width="112" height="101" />
-      </p>
-      <h2>SVG Image</h2>
-      <img src={cover} alt="React Cover" width="300" />
-      <ClickCounter />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
